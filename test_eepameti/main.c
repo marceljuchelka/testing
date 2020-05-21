deb8da86a15cc8e3b299b4deab5ecbb6215a3ff0
Marcel Juchelka <xer@seznam.cz>
2020-05-21 05:39:56 +0200

    start1

---
+++ test_eepameti/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 21. 5. 2020
+ *      Author: Marcel Juchelka
+ */
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+int main(void){
+	while(1){
+
+	}
+}
