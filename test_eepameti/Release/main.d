d9c6a4c7c8cca581092ef5b2f99c509f4d0cecc9
Marcel Juchelka <xer@seznam.cz>
2020-05-21 08:33:40 +0200

    test2

--- test_eepameti/Release/main.d
+++ test_eepameti/Release/main.d
@@ -1,1 +1,3 @@
-main.o: ../main.c
+main.o: ../main.c ../UART/uart.h
+
+../UART/uart.h:
--- test_eepameti/main.c
+++ test_eepameti/main.c
@@ -9,9 +9,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
+#include "UART/uart.h"
+
+
+void odesli( unsigned char data );
 
 int main(void){
+	uart_init();
+
+	UBRR0 = 103;
+	UCSR0B = (1<<TXEN0);  // zapnout vysílaè
+// 1 stop bit,žádná parita, 8bit zpráva
+	UCSR0C =  (1<<UCSZ01) | (1<<UCSZ00);
+	DDRD = 0;// vstup pro tlaèítko
+	DDRB |=	(1<<PB5);
+	PORTB |= (1<<PB5);
+	char text[]= "AHOJ";
+
+uint32_t rychlost = 1000;
 	while(1){
 
+		if (!(PIND& (1<<PD7))) rychlost = 250000;
+		else rychlost = 1000000;
+		PORTB^= (1<<PB5);
+		while(rychlost--);
+uart_putchar(&text,'\n');
 	}
+
+}
+
+void odesli( unsigned char data ){
+
 }
