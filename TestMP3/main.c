0e6bde0368281ae39002b60bd0b816a03c013274
Marcel Juchelka <xer@seznam.cz>
2020-07-22 08:51:00 +0200

    start

---
+++ TestMP3/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 21. 7. 2020
+ *      Author: Marcel Juchelka
+ */
+
+
+#include <avr/io.h>
+#include "MK_I2C/mk_i2c.h"
+#include "MK_LCD/mk_lcd44780.h"
+
+
+int main(void){
+	while(1){
+
+	}
+}
